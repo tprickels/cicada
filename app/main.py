8ab993ee637b8d657a0c3b6be23726205cc8ed1a
tprickels <32598015+tprickels@users.noreply.github.com>
2024-07-19 22:07:57 -0700

    fixing linting

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
+'''
+Basic app to do something on a schedule.
+'''
 import random
-import schedule
 import time
 import logging
 
+import schedule
+
 
 def math():
+    '''
+    Log a Buzz and Random Number.
+    '''
     logging.warning('buzz')
     logging.warning(random.randrange(500))
 
+
 def main():
+    '''
+    Run the math method every 10 seconds forever.
+    '''
     schedule.every(10).seconds.do(math)
     while True:
         schedule.run_pending()
         time.sleep(1)
-    
+
 
 if __name__ == '__main__':
-   main()
+    main()
